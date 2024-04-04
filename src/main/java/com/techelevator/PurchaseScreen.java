7a97615582af059fa8dda9fd3e5d14e8022e67c9
Jesse Stewart <jesse09bl@gmail.com>
2024-04-04 16:52:43 -0400

    Working into PurchaseScreen

--- src/main/java/com/techelevator/PurchaseScreen.java
+++ src/main/java/com/techelevator/PurchaseScreen.java
@@ -1,12 +1,44 @@
 package com.techelevator;
+import com.techelevator.VendingMachine;
+import com.techelevator.Items;
 
-public class PurchaseScreen {
-    public void onChosenScreen(VendingMachine vendingMachine) {
+public class PurchaseScreen extends VendingMachine{
+    private static final String OPTION_FEED = "Feed Money";
+    private static final String OPTION_SELECT = "Select Product";
+    private static final String OPTION_FINISH = "Finish Transaction";
+    private static final String[] OPTIONS = {OPTION_FEED, OPTION_SELECT, OPTION_FINISH};
 
+    private double currentMoneyProvided;
+    public double getCurrentMoneyProvided(){
+        return currentMoneyProvided;
     }
 
+    public void onChosenScreen(VendingMachine vendingMachine) {
+
+        while (true) {
+
+            String selectedOption;
+
+            selectedOption = MenuDisplay.prompt(OPTIONS);
+            if (selectedOption.equals("Feed Money")) {
+                getCurrentMoneyProvided();
+            } else if (selectedOption.equals("Select Product")) {
+                System.out.println(inventory);
+            } else {
+                
+
+                break;
+            }
+        }
 
 
 
+
+
+
+
+
+
+    }
 
 }
