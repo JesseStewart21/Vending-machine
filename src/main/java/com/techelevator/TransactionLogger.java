a8e91a42f8696a64cf0dfce5aaeee252bd2ffc6f
Melvina Jacobs <mjacobs1341@gmail.com>
2024-04-08 19:27:21 -0400

    transaction log

--- src/main/java/com/techelevator/TransactionLogger.java
+++ src/main/java/com/techelevator/TransactionLogger.java
@@ -1,25 +1,31 @@
 package com.techelevator;
 import java.io.*;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatterBuilder;
 import java.util.Date;
 import java.time.format.DateTimeFormatter;
 import java.io.PrintWriter;
 public class TransactionLogger {
 
-    public void logTransactions (double amount, double newBalance) throws IOException {
+    public void logTransactions (double amount, double newBalance) {
 
 
-        String date = new String ("MM/dd/yyyy");
-        String time = new String("hh:mm:ss");
-        String timeStampDetails = (date + time + feed Money : currentMoneyProvided + newBalance);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a");
+        LocalDateTime date = LocalDateTime.now();
+        String transactionType = "feed money";
+        String timeStampDetails = date.format(formatter) + " " + transactionType + ": $" + String.format("%.2f, amount") + " $" + String.format("%.2f", newBalance);
 
-        try (PrintWriter writer = new PrintWriter(new FileWriter("Log.txt")) {
-            writer.printIn (timeStampDetails);
+
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter("Log.txt"))) {
+            writer.println(timeStampDetails);
 
         } catch (IOException ex) {
 
         }
+
+
 
 
 
--- src/test/java/com/techelevator/UnitTest1.java
+++ src/test/java/com/techelevator/UnitTest1.java
@@ -18,25 +18,9 @@ public class UnitTest1 {
 	}
 
 
-	@Test
-	public void testBeginApplication(){
-
-		MenuDisplay mt  = new MenuDisplay();
-		DisplayScreen ds = new DisplayScreen ();
-		PurchaseScreen ps = new PurchaseScreen ();
-
-	}
 
 
-	//public void testConstructor() throws FileNotFoundException {
 
-
-	public void testOnChosenScreen() {
-		VendingMachine vm = new VendingMachine();
-		List<Items> inventory = new ArrayList<>();
-		inventory.add(new Items("location", "productName", price, "type", 5));
-
-	}
 
 
 
